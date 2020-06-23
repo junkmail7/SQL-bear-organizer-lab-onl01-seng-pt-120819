5ca5c9c5784a96d69a313a7f4dc5de5e0da9cec1
William Pannell <lpann5@gmail.com>
2020-06-23 00:20:28 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (0, 'Mr. Chocolate', 3, 'male', 'brown', 'sad', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (0, 'Rowdy', 5, 'male', 'black', 'sad', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (0, 'Tabitha', 6, 'female', 'green', 'sad', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (0, 'Sergeant Brown', 3, 'purple', 'brown', 'sad', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (0, 'Melissa', 7, 'female', 'brown', 'sad', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (0, 'Grinch', 2, 'male', 'turquoise', 'sad', 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (0, 'Wendy', 4, 'female', 'brown', 'sad', 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (0, '', 3, 'male', 'brown', 'sad', 1);
